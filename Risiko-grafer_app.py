50fb73b3d7fee7214256f1769c777ca8eefade18
FrederikSondergaard <frederik.sondergaard333@gmail.com>
2025-07-01 13:08:49 +0000

    Opdateret

--- Risiko-grafer_app.py
+++ Risiko-grafer_app.py
@@ -158,7 +158,7 @@ ax1.set_title("FORVENTET FORMUEUDVIKLING", pad=15, fontsize=20, fontweight='bold
 ax1.set_xlabel("Tidshorisont", labelpad=15)
 ax1.set_ylabel(f"Porteføljeværdi (start = {beløb:,.0f} kr.)", labelpad=15)
 ax1.yaxis.set_major_formatter(FuncFormatter(tusind_millioner_formatter))
-ax1.set_ylim(min(p2_5)-0.4 * beløb, max(p97_5) + 0.5 * beløb)
+ax1.set_ylim(min(p2_5)-0.4 * min(p2_5), max(p97_5) + 0.5 * max(p97_5))
 ax1.axhline(0, color="gray", linewidth=0.8)  # Tegn x-aksen ved y=0
 ax1.legend(loc="upper left")
 ax1.grid(False)  # Fjern gridlines
